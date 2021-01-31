fd93c4b04a4a36a80dc22ac6c78a8af44a2efb4e
krizovakk <krizovakk@gmail.com>
2021-01-31 14:53:39 +0100

    cattle plots

--- vac.R
+++ vac.R
@@ -3,20 +3,199 @@
 # install.packages("tidyverse")
 # install.packages("readxl")
 # install.packages("reshape2")
+# install.packages("RColorBrewer")
 require(tidyverse)
 require(readxl)
 require(reshape2)
+require(RColorBrewer)
 
+nasepaleta <- c("darkgreen", "limegreen", "darkturquoise", "dodgerblue4",
+                 "gray60","burlywood4", "gray11")
+# KRAVA load -----------------------------------------------------------
+
+kra_pr <- read_excel("red/krava.xlsx", sheet = 2)
+kra_roh <- read_excel("red/krava.xlsx", sheet = 3)
+kra_ud <- read_excel("red/krava.xlsx", sheet = 4)
+kra_hc <- read_excel("red/krava.xlsx", sheet = 5)
+kra_y <- read_excel("red/krava.xlsx", sheet = 6)
+
+# PRASE load -------------------------------------------------------------------
+
+pra_pr <- read_excel("red/prase.xlsx", sheet = 2)
+pra_roh <- read_excel("red/prase.xlsx", sheet = 3)
+pra_ud <- read_excel("red/prase.xlsx", sheet = 4)
+pra_hc <- read_excel("red/prase.xlsx", sheet = 5)
+pra_y <- read_excel("red/prase.xlsx", sheet = 6)
+
+# HNOJE load -------------------------------------------------------------------
+
+npk_pr <- read_excel("red/npk.xlsx", sheet = 2)
+npk_roh <- read_excel("red/npk.xlsx", sheet = 3)
+npk_ud <- read_excel("red/npk.xlsx", sheet = 4)
+npk_hc <- read_excel("red/npk.xlsx", sheet = 5)
+npk_y <- read_excel("red/npk.xlsx", sheet = 6)
+
+
+# KRAVA VARIANTS  ---------------------------------------------------------------
+
+kra_hc$var[kra_hc$var == 1] <- "fix"
+kra_hc$var[kra_hc$var == 2] <- "fix_sol"
+kra_hc$var[kra_hc$var == 3] <- "c"
+kra_hc$var[kra_hc$var == 4] <- "sol"
+
+kra_pr$var[kra_pr$var == 1] <- "fix"
+kra_pr$var[kra_pr$var == 2] <- "fix_sol"
+kra_pr$var[kra_pr$var == 3] <- "c"
+kra_pr$var[kra_pr$var == 4] <- "sol"
+
+kra_roh$var[kra_roh$var == 1] <- "fix"
+kra_roh$var[kra_roh$var == 2] <- "fix_sol"
+kra_roh$var[kra_roh$var == 3] <- "c"
+kra_roh$var[kra_roh$var == 4] <- "sol"
+
+kra_ud$var[kra_ud$var == 1] <- "fix"
+kra_ud$var[kra_ud$var == 2] <- "fix_sol"
+kra_ud$var[kra_ud$var == 3] <- "c"
+kra_ud$var[kra_ud$var == 4] <- "sol"
+
+kra_y$var[kra_y$var == 1] <- "fix"
+kra_y$var[kra_y$var == 2] <- "fix_sol"
+kra_y$var[kra_y$var == 3] <- "c"
+kra_y$var[kra_y$var == 4] <- "sol"
+
+# PRASE VARIANTS ----------------------------------------------------------
+
+pra_hc$var[pra_hc$var == 7] <- "fix"
+pra_hc$var[pra_hc$var == 8] <- "fix_sol"
+pra_hc$var[pra_hc$var == 9] <- "c"
+pra_hc$var[pra_hc$var == 10] <- "sol"
+
+pra_pr$var[pra_pr$var == 7] <- "fix"
+pra_pr$var[pra_pr$var == 8] <- "fix_sol"
+pra_pr$var[pra_pr$var == 9] <- "c"
+pra_pr$var[pra_pr$var == 10] <- "sol"
+
+pra_roh$var[pra_roh$var == 7] <- "fix"
+pra_roh$var[pra_roh$var == 8] <- "fix_sol"
+pra_roh$var[pra_roh$var == 9] <- "c"
+pra_roh$var[pra_roh$var == 10] <- "sol"
+
+pra_ud$var[pra_ud$var == 7] <- "fix"
+pra_ud$var[pra_ud$var == 8] <- "fix_sol"
+pra_ud$var[pra_ud$var == 9] <- "c"
+pra_ud$var[pra_ud$var == 10] <- "sol"
+
+pra_y$var[pra_y$var == 7] <- "fix"
+pra_y$var[pra_y$var == 8] <- "fix_sol"
+pra_y$var[pra_y$var == 9] <- "c"
+pra_y$var[pra_y$var == 10] <- "sol"
+
+# NPK VARIANTS ----------------------------------------------------------
+
+npk_hc$var[npk_hc$var == 3] <- "c_fym"
+npk_hc$var[npk_hc$var == 5] <- "sol"
+npk_hc$var[npk_hc$var == 6] <- "c"
+npk_hc$var[npk_hc$var == 9] <- "p_fym"
+
+npk_pr$var[npk_pr$var == 3] <- "c_fym"
+npk_pr$var[npk_pr$var == 5] <- "sol"
+npk_pr$var[npk_pr$var == 6] <- "c"
+npk_pr$var[npk_pr$var == 9] <- "p_fym"
+
+npk_roh$var[npk_roh$var == 3] <- "c_fym"
+npk_roh$var[npk_roh$var == 5] <- "sol"
+npk_roh$var[npk_roh$var == 6] <- "c"
+npk_roh$var[npk_roh$var == 9] <- "p_fym"
+
+npk_ud$var[npk_ud$var == 3] <- "c_fym"
+npk_ud$var[npk_ud$var == 5] <- "sol"
+npk_ud$var[npk_ud$var == 6] <- "c"
+npk_ud$var[npk_ud$var == 9] <- "p_fym"
+
+npk_y$var[npk_y$var == 3] <- "c_fym"
+npk_y$var[npk_y$var == 5] <- "sol"
+npk_y$var[npk_y$var == 6] <- "c"
+npk_y$var[npk_y$var == 9] <- "p_fym"
+
+# SELECT ------------------------------------------------------------------
+
+# penres
+
+kra_pr <- kra_pr %>%
+  select(var, year, d04, d08, d12, d16, d20)
+pra_pr <- pra_pr %>%
+  select(var, year, d04, d08, d12, d16, d20)
+npk_pr <- npk_pr %>%
+  select(var, year, d04, d08, d12, d16, d20)
+
+# roh
+
+kra_roh <- kra_roh %>%
+  select(var, year, roh)
+pra_roh <- pra_roh %>%
+  select(var, year, roh)
+npk_roh <- npk_roh %>%
+  select(var, year, roh)
+
+# unitd
+
+kra_ud <- kra_ud %>%
+  select(var, year, unitd)
+pra_ud <- pra_ud %>%
+  select(var, year, unitd)
+npk_ud <- npk_ud %>%
+  select(var, year, unitd)
+
+# hcon
+
+kra_hc <- kra_hc %>%
+  select(var, year, hcon)
+pra_hc <- pra_hc %>%
+  select(var, year, hcon)
+npk_hc <- npk_hc %>%
+  select(var, year, hcon)
+
+# yield
+
+kra_y <- kra_y %>%
+  select(var, s15, s16, s17, s18, s19, s20)
+pra_y <- pra_y %>%
+  select(var, s15, s16, s17, s18, s19, s20)
+npk_y <- npk_y %>%
+  select(var, s15, s16, s17, s18, s19, s20)
 
 # PENRES ------------------------------------------------------------------
 
-pen <- read_excel("red/korbic.xlsx", sheet = 1)
+kra_pr$var <- factor(kra_pr$var)
+pra_pr$var <- factor(pra_pr$var)
+npk_pr$var <- factor(npk_pr$var)
 
-# pen$var[pen$var == 7] <- "ZF"
-# pen$var[pen$var == 8] <- "ZF_SOL"
-# pen$var[pen$var == 9] <- "C"
-# pen$var[pen$var == 10] <- "SOL"
+kra_pr$year <- factor(kra_pr$year)
+pra_pr$year <- factor(pra_pr$year)
+npk_pr$year <- factor(npk_pr$year)
 
+# krava
+
+kra_pr_div <- kra_pr %>% 
+  melt(id.vars = c("var", "year"), variable.name = ("depth"), value.name = "penres") %>% 
+  mutate(div = penres*100-100) %>% 
+  filter(var %in% c("sol", "fix", "fix_sol"))
+kra_pr_div$year <- factor(kra_pr_div$year)
+
+dept <- c("20", "16", "12", "8", "4")
+kra_pr_div$depth <- factor(kra_pr_div$depth, levels = c("d20", "d16", "d12", "d08", "d04"), labels = dept)
+
+ggplot(kra_pr_div, aes(depth, div, fill = year))+
+  geom_bar(position = "dodge", stat = "summary", fun.y = "mean", width = 0.6)+
+  scale_fill_manual(values = nasepaleta)+
+  facet_grid(year ~ var, scales="fixed")+
+  coord_flip()+
+  labs(x = "Depth [cm]", y = "Cone Index [%]", title = "Cattle Manure")+
+  theme_minimal()+
+  theme(legend.position = "none")
+ggsave("plots/kra_pr.png", device = "png", width = 5, height = 6, dpi = 500)
+
+###
 pen$var <- factor(pen$var, levels = c(0, 1, 2, 3, 4), 
                   labels = c("0", "10", "20", "30", "40"))
 
@@ -69,274 +248,75 @@ pairwise.wilcox.test(pen$d20, pen$var,
 
 
 
-# RBD ---------------------------------------------------------------------
+# ROH ---------------------------------------------------------------------
 
-rbd <- read_excel("red/korbic.xlsx", sheet = 2)
+kra_roh$var <- factor(kra_roh$var)
+pra_roh$var <- factor(pra_roh$var)
+npk_roh$var <- factor(npk_roh$var)
 
-rbd$var <- factor(rbd$var, levels = c(0, 1, 2, 3, 4), 
-                     labels = c("0", "10", "20", "30", "40"))
+kra_roh$year <- factor(kra_roh$year)
+pra_roh$year <- factor(pra_roh$year)
+npk_roh$year <- factor(npk_roh$year)
 
-ggplot(rbd, aes(var, roh))+
-  geom_bar(stat = "summary", fun.y = "mean", width = 0.6)+
-  labs(y = expression("Reduced Bulk Density [g "~ cm^-3~"]"), 
-       x = expression("Digestate Dose [ t "~ha^-1~"]"), title = "")+
+# krava
+
+kra_roh_div <- kra_roh %>% 
+  mutate(div = roh*100-100) %>% 
+  filter(var %in% c("sol", "fix", "fix_sol"))
+
+ggplot(kra_roh_div, aes(var, div, fill = year))+
+  geom_bar(position = "dodge", stat = "summary", fun.y = "mean", width = 0.6)+
+  scale_fill_manual(values = nasepaleta)+
+  labs(y = "Reduced Bulk Density [%]", 
+       x = "", title = "Cattle Manure", fill = "Season")+
   theme_minimal()
-ggsave("plots/rbd.png", device = "png", width = 6, height = 3, dpi = 500)
+ggsave("plots/kra_roh.png", device = "png", width = 6, height = 3, dpi = 500)
 
-# hraju si s grafem, aby byl vyraznejsi
+# UNIT DRAFT --------------------------------------------------------------
 
+kra_ud$var <- factor(kra_ud$var)
+pra_ud$var <- factor(pra_ud$var)
+npk_ud$var <- factor(npk_ud$var)
 
-ggplot(rbd, aes(var, roh))+
-  geom_bar(stat = "summary", fun.y = "mean", width = 0.6)+
-  labs(y = expression("Reduced Bulk Density [g "~ cm^-3~"]"), 
-       x = expression("Digestate Dose [ t "~ha^-1~"]"), title = "")+
-  coord_cartesian(ylim = c(1, 1.50))+
+kra_ud$year <- factor(kra_ud$year)
+pra_ud$year <- factor(pra_ud$year)
+npk_ud$year <- factor(npk_ud$year)
+
+# krava
+
+kra_ud_div <- kra_ud %>% 
+  mutate(div = unitd*100-100) %>% 
+  filter(var %in% c("sol", "fix", "fix_sol"))
+
+ggplot(kra_ud_div, aes(var, div, fill = year))+
+  geom_bar(position = "dodge", stat = "summary", fun.y = "mean", width = 0.6)+
+  scale_fill_manual(values = nasepaleta)+
+  labs(y = "Unit Draft [%]", 
+       x = "", title = "Cattle Manure", fill = "Season")+
   theme_minimal()
-ggsave("plots/rbd_scale.png", device = "png", width = 6, height = 3, dpi = 500)
+ggsave("plots/kra_ud.png", device = "png", width = 6, height = 3, dpi = 500)
 
-# EMMISIONS ---------------------------------------------------------------
 
-em <- read_excel("red/korbic.xlsx", sheet = 3)
+# HYDRAULIC COND -----------------------------------------------------------
 
-eml <- em %>% 
-  select(var, dose, amm, co2, meth) %>% 
-  melt(id.vars = c("var", "dose"), variable.name = ("gas"), value.name = "emmision")
+kra_hc$var <- factor(kra_hc$var)
+pra_hc$var <- factor(pra_hc$var)
+npk_hc$var <- factor(npk_hc$var)
 
-eml$var <- as.factor(eml$var)
-eml$dose <- as.factor(eml$dose)
+kra_hc$year <- factor(kra_hc$year)
+pra_hc$year <- factor(pra_hc$year)
+npk_hc$year <- factor(npk_hc$year)
 
-# ammonium 
+# krava
 
-amm <- eml %>% 
-  filter(gas == "amm")
+kra_hc_div <- kra_hc %>% 
+  mutate(div = hcon*100-100) %>% 
+  filter(var %in% c("sol", "fix", "fix_sol"))
 
-ggplot(amm, aes(dose, emmision))+
-  geom_boxplot()+
-  labs(y = expression(paste("N",H[3]," [kg ",m^-2 , y^-1,"]")), 
-       x = expression("Digestate Dose [ t "~ha^-1~"]"))+
+ggplot(kra_hc_div, aes(var, div, fill = year))+
+  geom_bar(position = "dodge", stat = "summary", fun.y = "mean", width = 0.6)+
+  scale_fill_manual(values = nasepaleta)+
+  labs(y = "Saturated Hydraulic Conductivity [%]", 
+       x = "", title = "Cattle Manure", fill = "Season")+
   theme_minimal()
-ggsave("plots/ammonia.png", device = "png", width = 6, height = 3, dpi = 500)
-
-shapiro.test(amm$emmision)
-hist(amm$emmision)
-
-aov1 <- aov(emmision ~ var, data = amm)
-summary(aov1)
-
-# co2
-
-co2 <- eml %>% 
-  filter(gas == "co2")
-
-ggplot(co2, aes(dose, emmision))+
-  geom_boxplot()+
-  labs(y = expression(paste("C",O[2]," [kg ",m^-2 , y^-1,"]")), 
-       x = expression("Digestate Dose [ t "~ha^-1~"]"))+
-  theme_minimal()
-ggsave("plots/co2.png", device = "png", width = 6, height = 3, dpi = 500)
-
-shapiro.test(co2$emmision)
-hist(co2$emmision)
-
-aov2 <- aov(emmision ~ var, data = co2)
-summary(aov2)
-
-TukeyHSD(aov2)
-
-# meth
-
-meth <- eml %>% 
-  filter(gas == "meth")
-
-ggplot(meth, aes(var, emmision))+
-  geom_boxplot()+
-  labs(y = expression(paste("C",H[4]," [kg ",m^-2 , y^-1,"]")), 
-       x = expression("Digestate Dose [ t "~ha^-1~"]"))+
-  theme_minimal()
-ggsave("plots/methane.png", device = "png", width = 6, height = 3, dpi = 500)
-
-shapiro.test(meth$emmision)
-hist(meth$emmision)
-
-aov3 <- aov(emmision ~ var, data = meth)
-summary(aov3)
-
-TukeyHSD(aov3)
-
-# NDVI --------------------------------------------------------------------
-
-ndvi <- read_excel("red/ndvi.xlsx", sheet = 1)
-
-ndvi <- ndvi %>% 
-  filter(dateid %in% c("a", "b", "c", "d", "e", "f"))
-
-ndvi$var <- factor(ndvi$var, levels = c("D0", "D10", "D20", "D30", "D40"), 
-                  labels = c("0", "10", "20", "30", "40"))
-ndvi$date <- as.factor(ndvi$date)
-
-ggplot(ndvi, aes(date, ndvi, color = var))+
-  geom_boxplot()+
-  labs(y = "NDVI", 
-       x = "Date")+
-  scale_color_discrete(expression("Digestate Dose [ t "~ha^-1~"]"))+
-  theme_minimal()+
-  theme(legend.position = "top")
-ggsave("plots/ndvi_date.png", device = "png", width = 8, height = 4, dpi = 500)
-
-ggplot(ndvi, aes(var, ndvi, color = date))+
-  geom_boxplot()+
-  labs(y = "NDVI", 
-       x = expression("Digestate Dose [ t "~ha^-1~"]"))+
-  theme_minimal()
-ggsave("plots/ndvi_dose.png", device = "png", width = 6, height = 3, dpi = 500)
-
-# GNDVI -------------------------------------------------------------------
-
-gndvi <- read_excel("red/gndvi.xlsx", sheet = 1)
-
-gndvi <- gndvi %>% 
-  filter(dateid %in% c("a", "b", "c", "d", "e", "f"))
-
-gndvi$var <- factor(gndvi$var, levels = c("D0", "D10", "D20", "D30", "D40"), 
-                   labels = c("0", "10", "20", "30", "40"))
-gndvi$date <- as.factor(gndvi$date)
-
-ggplot(gndvi, aes(date, gndvi, color = var))+
-  geom_boxplot()+
-  labs(y = "GNDVI", 
-       x = "Date")+
-  scale_color_discrete(expression("Digestate Dose [ t "~ha^-1~"]"))+
-  theme_minimal()+
-  theme(legend.position = "top")
-ggsave("plots/gndvi_date.png", device = "png", width = 8, height = 4, dpi = 500)
-
-ggplot(gndvi, aes(var, gndvi, color = date))+
-  geom_boxplot()+
-  labs(y = "GNDVI", 
-       x = expression("Digestate Dose [ t "~ha^-1~"]"))+
-  theme_minimal()
-ggsave("plots/gndvi_dose.png", device = "png", width = 6, height = 3, dpi = 500)
-
-# SAVI --------------------------------------------------------------------
-
-savi <- read_excel("red/savi.xlsx", sheet = 1)
-
-savi <- savi %>% 
-  filter(dateid %in% c("a", "b", "c", "d", "e", "f"))
-
-savi$var <- factor(savi$var, levels = c("D0", "D10", "D20", "D30", "D40"), 
-                    labels = c("0", "10", "20", "30", "40"))
-savi$date <- as.factor(savi$date)
-
-ggplot(savi, aes(date, savi, color = var))+
-  geom_boxplot()+
-  labs(y = "SAVI", 
-       x = "Date")+
-  scale_color_discrete(expression("Digestate Dose [ t "~ha^-1~"]"))+
-  theme_minimal()+
-  theme(legend.position = "top")
-ggsave("plots/savi_date.png", device = "png", width = 8, height = 4, dpi = 500)
-
-ggplot(savi, aes(var, savi, color = date))+
-  geom_boxplot()+
-  labs(y = "SAVI", 
-       x = expression("Digestate Dose [ t "~ha^-1~"]"))+
-  theme_minimal()
-ggsave("plots/savi_dose.png", device = "png", width = 6, height = 3, dpi = 500)
-
-
-# RS data -----------------------------------------------------------------
-
-# ANOVA repeated measures
-
-summary(aov(ndvi ~ var + Error(date), data=ndvi))
-summary(aov(ndvi ~ var * date, data=ndvi))
-        
-
-a1 <- aov(ndvi ~ var * date, data=ndvi)
-par.mfrow= c(2,2)
-plot(a1, which = 1:4)
-
-shapiro.test(ndvi$ndvi)
-hist(ndvi$ndvi)
-hist(log(ndvi$ndvi))
-shapiro.test(log(ndvi$ndvi))
-
-
-# RM ANOVA ----------------------------------------------------------------
-
-#https://neuropsychology.github.io/psycho.R/2018/05/01/repeated_measure_anovas.html
-
-install.packages("lmerTest")
-install.packages("psycho")
-require(lmerTest)
-require(psycho)
-
-fit <- lmer(ndvi ~ var + (1|date), data=ndvi)
-anova(fit)
-
-results <- analyze(fit)  # Error in analyze(fit) : could not find function "analyze"
-
-# https://www.youtube.com/watch?v=www1JPobnx0&ab_channel=AGRONInfo-Tech
-
-yb <- aov(ndvi ~ var + Error(date/var), data=ndvi)
-summary(yb)
-
-require(stats)
-
-pairwise.t.test(ndvi$ndvi, ndvi$var, p.adjust.method = "bonferroni")
-pairwise.t.test(ndvi$ndvi, ndvi$var, p.adjust.method = "BH")
-pairwise.t.test(ndvi$ndvi, ndvi$var, p.adjust.method = "hochberg")
-
-# https://www.datanovia.com/en/lessons/repeated-measures-anova-in-r/#one-way-repeated-measures-anova
-
-install.packages("ggpubr")
-install.packages("rstatix")
-library(ggpubr)
-library(rstatix)
-
-# summary statistics
-
-ndvi %>% 
-  group_by(date) %>% 
-  get_summary_stats(ndvi, type = "mean_sd")
-
-# visualisation
-
-bxp <- ggboxplot(ndvi, x = "date", y = "ndvi", ass = "point")
-bxp <- ggboxplot(ndvi, x = "var", y = "ndvi", ass = "point")
-bxp
-
-# check assumptions
-
-ndvi %>% 
-  group_by(var) %>% 
-  identify_outliers(ndvi)
-
-ndvi %>% 
-  group_by(var) %>% 
-  shapiro_test(ndvi)
-
-ggqqplot(ndvi, "ndvi", facet.by = "var")
-
-# computation
-
-ndvi$ID <- seq.int(nrow(ndvi))
-
-res.aov <- anova_test(data = ndvi, dv = index, wid = var, within = date) # Error in lm.fit(x, y, offset = offset, singular.ok = singular.ok, ...) : 0 (non-NA) cases
-get_anova_table(res.aov)
-
-all(is.na(var))
-all(is.na(ndvi))
-all(is.na(y^trans))
-
-colnames(ndvi)[1] <- "index"
-
-# post-hoc test
-
-pwc <- ndvi %>%
-  pairwise_t_test(
-    index ~ var, paired = TRUE,
-    p.adjust.method = "bonferroni")
-  
-pwc
+ggsave("plots/kra_hc.png", device = "png", width = 6, height = 3, dpi = 500)
