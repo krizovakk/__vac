457dc00f945082947ee005cec3b861af185544be
krizovakk <krizovakk@gmail.com>
2021-03-28 22:31:27 +0200

    unitd kw done / labels to ggplot

--- sust_sol.R
+++ sust_sol.R
@@ -87,9 +87,9 @@ ggplot(dfud, aes(year, unitd, fill=var))+
        x = "", title = "", fill = "")+
   theme_classic(base_size = 15)+
   theme(text=element_text(family="Times New Roman"))
-ggsave("plots/ud.png", device = "png", width = 8, height = 4, dpi = 300)
+# ggsave("plots/ud.png", device = "png", width = 8, height = 4, dpi = 300)
 
-# AN 1 - within years
+# analysis ----------------------------------------------------------------
 
 ud15 <- ud %>% 
   filter(year == "2015")
@@ -103,41 +103,49 @@ ud19 <- ud %>%
   filter(year == "2019")
 ud20 <- ud %>% 
   filter(year == "2020")
- 
+
+# homogenity of variance check
+
+bartlett.test(unitd ~ var, data = ud15)
+bartlett.test(unitd ~ var, data = ud16)
+bartlett.test(unitd ~ var, data = ud17) # nope
+bartlett.test(unitd ~ var, data = ud18) # nope
+bartlett.test(unitd ~ var, data = ud19) # nope
+bartlett.test(unitd ~ var, data = ud20)
+
 # kw 
 
-kruskal.test(unitd ~ var, data = ud15)
+kruskal.test(unitd ~ var, data = ud15) # yep
 kruskal.test(unitd ~ var, data = ud16)
-kruskal.test(unitd ~ var, data = ud17)
-kruskal.test(unitd ~ var, data = ud18)
-kruskal.test(unitd ~ var, data = ud19)
-kruskal.test(unitd ~ var, data = ud20)
+kruskal.test(unitd ~ var, data = ud17) # yep
+kruskal.test(unitd ~ var, data = ud18) # yep
+kruskal.test(unitd ~ var, data = ud19) # yep
+kruskal.test(unitd ~ var, data = ud20) # yep
 
 # install.packages("pgirmess") # post-hoc testy
 require(pgirmess)
-# 
+ 
 kruskalmc(unitd ~ var, data = ud15, p=0.05)
-# kruskalmc(penres ~ var, data = c2_kraud, p=0.05)
-# kruskalmc(penres ~ var, data = c3_kraud, p=0.05)
+kruskalmc(unitd ~ var, data = ud17, p=0.05)
+kruskalmc(unitd ~ var, data = ud18, p=0.05)
+kruskalmc(unitd ~ var, data = ud19, p=0.05)
+kruskalmc(unitd ~ var, data = ud20, p=0.05)
 
-## one way ANOVA
+# ## one way ANOVA
+# 
+# simpud15 <- aov(unitd ~ var, data = ud15)
+# simpkraudc2 <- aov(udkn ~ var, data = c2_kraud)
+# simpkraudc3 <- aov(udkn ~ var, data = c3_kraud)
+# 
+# summary(simpud15)
+# 
+# TukeyHSD(simpud15)
+# # plot(TukeyHSD(simp20))
+# # install.packages("multcomp")
+# require(multcomp)
+# summary(glht(simpud15, linfct=mcp(var="Tukey")))
 
-bartlett.test(unitd ~ var, data = ud15)
-
-
-simpud15 <- aov(unitd ~ var, data = ud15)
-simpkraudc2 <- aov(udkn ~ var, data = c2_kraud)
-simpkraudc3 <- aov(udkn ~ var, data = c3_kraud)
-
-summary(simpud15)
-
-TukeyHSD(simpud15)
-# plot(TukeyHSD(simp20))
-# install.packages("multcomp")
-require(multcomp)
-summary(glht(simpud15, linfct=mcp(var="Tukey")))
-
-# APL 
+# analysis of year of application
 
 cud <- ud %>% 
   filter(var == "cSOL")
@@ -146,12 +154,63 @@ hud <- ud %>%
 pud <- ud %>% 
   filter(var == "pSOL")
 # 
+shapiro.test(cud$unitd) # nope
+shapiro.test(hud$unitd) # nope
+shapiro.test(pud$unitd) # nope
+
+boxplot(cud$unitd[cud$apl1], cud$unitd[cud$apl2]) 
+boxplot(hud$unitd[cud$apl1], cud$unitd[hud$apl2]) 
+boxplot(pud$unitd[cud$apl1], cud$unitd[pud$apl2]) 
+
+wilcox.test(cud$unitd[cud$apl1], cud$unitd[cud$apl2]) # no diff
+wilcox.test(hud$unitd[cud$apl1], cud$unitd[hud$apl2]) # no diff
+wilcox.test(pud$unitd[cud$apl1], cud$unitd[pud$apl2]) # no diff
+
 # t.test(cud$unitd[cud$apl1], cud$unitd[cud$apl2])
 # t.test(hud$unitd[cud$apl1], cud$unitd[hud$apl2])
 # t.test(pud$unitd[cud$apl1], cud$unitd[pud$apl2])
 
-boxplot(cud$unitd[cud$apl1], cud$unitd[cud$apl2])
-boxplot(hud$unitd[cud$apl1], cud$unitd[hud$apl2])
-boxplot(pud$unitd[cud$apl1], cud$unitd[pud$apl2])
 
+# pismenka pro Vaclava ----------------------------------------------------
+
+# install.packages("multcompView")
+# install.packages("pgirmess")
+require(multcompView)
+library(pgirmess)
+
+kruskal.test(unitd ~ var, data = ud15) 
+kmcud15 <- kruskalmc(unitd ~ var, data = ud15, p=0.05)
+mcud15=kmcud15$dif.com[,3]
+names(mcud15)=row.names(kmcud15$dif.com)
+multcompLetters(mcud15)
+
+kruskal.test(unitd ~ var, data = ud16) 
+kmcud16 <- kruskalmc(unitd ~ var, data = ud16, p=0.05)
+mcud16=kmcud16$dif.com[,3]
+names(mcud16)=row.names(kmcud16$dif.com)
+multcompLetters(mcud16)
+
+kruskal.test(unitd ~ var, data = ud17) 
+kmcud17 <- kruskalmc(unitd ~ var, data = ud17, p=0.05)
+mcud17=kmcud17$dif.com[,3]
+names(mcud17)=row.names(kmcud17$dif.com)
+multcompLetters(mcud17)
+
+kruskal.test(unitd ~ var, data = ud18) 
+kmcud18 <- kruskalmc(unitd ~ var, data = ud18, p=0.05)
+mcud18=kmcud18$dif.com[,3]
+names(mcud18)=row.names(kmcud18$dif.com)
+multcompLetters(mcud18)
+
+kruskal.test(unitd ~ var, data = ud19) 
+kmcud19 <- kruskalmc(unitd ~ var, data = ud19, p=0.05)
+mcud19=kmcud19$dif.com[,3]
+names(mcud19)=row.names(kmcud19$dif.com)
+multcompLetters(mcud19)
+
+kruskal.test(unitd ~ var, data = ud20) 
+kmcud20 <- kruskalmc(unitd ~ var, data = ud20, p=0.05)
+mcud20=kmcud20$dif.com[,3]
+names(mcud20)=row.names(kmcud20$dif.com)
+multcompLetters(mcud20)
 
