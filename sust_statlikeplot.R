a958a49c089b97e47232a090d8c9fe1a9aa4673b
Katerina Krizova <krizovakk@gmail.com>
2021-05-27 12:20:52 +0200

    statlike plot UD w/ letters finished

--- sust_statlikeplot.R
+++ sust_statlikeplot.R
@@ -56,8 +56,8 @@ ud$var <- factor(ud$var, levels = varlev, labels = varlab)
 ud$term <- factor(ud$term)
 
 ud <- ud %>%
-  mutate(sol_f = (var %in% c("4", "5", "10", "14"))) %>%
-  mutate(sol_t = (var %in% c("3", "6", "9", "13"))) 
+  mutate(sol_t = (var %in% c("4", "5", "10", "14"))) %>%
+  mutate(sol_f = (var %in% c("3", "6", "9", "13"))) 
 ud <- ud %>%
   mutate(sol = case_when(sol_f == "TRUE" ~ "FALSE", 
                          sol_t == "TRUE" ~ "TRUE"))
@@ -67,6 +67,7 @@ ud <- ud %>%
                          var %in% c("5","6") ~ "npk", 
                          var %in% c("9","10") ~ "pig", 
                          var %in% c("13","14") ~ "poultry"))
+ud$duo <- factor(ud$duo, levels = c("npk", "cattle", "pig", "poultry"))
 
 dfud <- data_summary(ud, varname="unitd", 
                      groupnames=c("term", "var", "sol", "duo"))
@@ -75,19 +76,31 @@ write_xlsx(dfud,"prstat.xlsx") # package "writexl"
 
 # STATISTICA-like plot ----------------------------------------------------
 
+lab_let <- c("a","a","a", "a","a","a",
+           "ab","ab","b","ab","ab","a",
+           "ac","c","a","a","b","b",
+           "ab","ab","ab","b","a","c")
+
 ggplot(dfud, aes(term, unitd, group=sol))+
-  geom_line(aes(color = sol), size = 1, position=position_dodge(width = .5))+
-  geom_errorbar(aes(ymin=unitd-sd, ymax=unitd+sd, color = sol), width=.5, size = 1, position=position_dodge(width = .5))+
-  geom_point(aes(color = sol), size = 2, position=position_dodge(width = .5))+
+  geom_line(aes(linetype = sol), size = .7, position=position_dodge(width = 1))+
+  geom_errorbar(aes(ymin=unitd-sd, ymax=unitd+sd), width=.5, size = .5, position=position_dodge(width = 1))+ # linetype = sol
+  geom_point(size = 2, position=position_dodge(width = 1))+ # aes(shape = sol), 
   facet_grid(. ~ dfud$duo)+
-  # geom_text(aes(y = 123, label = labelkwud),
-  #           size = 3, position=position_dodge(0.9))+
-  # scale_fill_brewer(palette = "Greys")+
   labs(y = expression("Unit Draft [ kN"~ m^-2~"]"), 
        x = "", title = "", fill = "")+
-  scale_color_discrete(name="SOL")+
-  theme_classic(base_size = 15)+
-  theme(text=element_text(family="Times New Roman"), axis.text.x = element_text(size = 9))
-ggsave("plots/statlike_ud.png", device = "png", width = 8, height = 4, dpi = 300)
+  coord_cartesian(ylim = c(85, 115))+
+  scale_linetype_discrete(name="SOL")+
+  guides(shape = FALSE)+
+  geom_text(aes(y = 113, label = lab_let),
+            size = 5, position=position_dodge(1))+
+  theme_classic(base_size = 20)+ # base_size = 20
+  theme(text=element_text(family="Times New Roman")) # axis.text.x = element_text(size = 11)
+
+# ggsave("plots/statlike_ud.png", device = "png", width = 10, height = 5, dpi = 300)
+#ggsave("plots/statlike_ud_2.png", device = "png", width = 10, height = 6, dpi = 300)
+#ggsave("plots/statlike_ud_3.png", device = "png", width = 10, height = 6, dpi = 300)
+
+
+
 
 
